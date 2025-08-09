deee3b6f4f4447b7bfb8d75c59a817985913738d
m2rcc0 <m2rcc0@gmail.com>
2025-08-09 12:02:48 +0300

    Add main chat page

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/supabaseClient';
+
+export default function Home() {
+  // Chat logic here
+  return <div>Welcome to the Messenger App!</div>;
+}
